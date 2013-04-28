3e24486f1a3a6da8bd79c90a89cb5ea99ca61cc2
XXIII <xxiii@gmx.eu>
2013-04-28 02:01:15 +0200

    test

--- src/main/java/com/saugstation/boerse/Download.java
+++ src/main/java/com/saugstation/boerse/Download.java
@@ -8,7 +8,7 @@ import java.net.URL;
 public class Download {
 
     public static void main(String[] args) throws IOException {
-        URL url = new URL("BLUBBERhttps://www.google.com/finance/historical?q=FRA%3ADTE&startdate=Jan+1%2C+2000&enddate=Apr+27%2C+2013&output=csv");
+        URL url = new URL("vvvvBLUBBERhttps://www.google.com/finance/historical?q=FRA%3ADTE&startdate=Jan+1%2C+2000&enddate=Apr+27%2C+2013&output=csv");
         String s;
         BufferedReader r = new BufferedReader(new InputStreamReader(url.openStream()));
         while ((s = r.readLine()) != null) {
